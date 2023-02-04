ccd7f4abc69c4dc32a05e05b72a27c749acddbb1
Syu <mirzamrion@gmail.com>
2023-02-04 23:41:43 +0900

    Initial Commit

---
+++ shoku_ver2/shoku_ver2App.swift
@@ -0,0 +1,17 @@
+//
+//  shoku_ver2App.swift
+//  shoku_ver2
+//
+//  Created by 村瀬嵩 on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct shoku_ver2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
